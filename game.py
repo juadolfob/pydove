a8e8f7c4489a134ba1b875d25f04824b5be39763
kitmi <juadolfog@gmail.com>
2020-07-23 09:25:03 -0700

    first

---
+++ game.py
@@ -0,0 +1,36 @@
+
+class game:
+
+    def __init__(game,strategy,strategy_domain):
+        # not necessary maybe
+        self.discrete=true
+        self.game_bounds
+        self.game_matrix=game
+        self.strategy=self.normalize_strategy()
+
+    def dominated_strategy(self):
+        pass
+
+    def game_type(self):
+        pass
+
+    #saddle points, Nash equilibrium, or optimal strategies
+    def optimal_strategies(self):
+        pass
+
+    def value(self):
+        pass
+
+    def pure_strategies(self):
+        pass
+
+    def best_response(self):
+        pass
+
+    def normalize_strategy():
+        pass
+
+
+
+
+#p 27
