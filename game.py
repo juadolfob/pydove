6e2e392eefae41889b923942ef8503406179458f
kitmi <juadolfog@gmail.com>
2020-07-25 18:55:03 -0700

    -- mixed and pure -- saddle-points and values added

--- game.py
+++ game.py
@@ -1,12 +1,38 @@
+import numpy as np
+import sympy as sym
+
+
+def _validate_game_matrix(game_matrix):
+    is_mxn = all(len(row) == len(game_matrix[0]) for row in game_matrix)
+    return is_mxn
+
+
+def _validate_strategy(len, strategy):
+    has_regular_Iterables = all(len(row) == len(strategy[0]) for row in strategy)
+    return has_regular_Iterables
+
 
 class game:
 
-    def __init__(game,strategy,strategy_domain):
+    def __init__(self, game_matrix, strategy=None):
         # not necessary maybe
-        self.discrete=true
-        self.game_bounds
-        self.game_matrix=game
-        self.strategy=self.normalize_strategy()
+        if _validate_game_matrix(game_matrix):
+            self.game_matrix = game_matrix
+        if strategy:
+            self.strategy = self._normalize_strategy(strategy)
+
+    def _normalize_strategy(self, strategy):
+        empty_s = [0] * len(strategy)
+        for s in range(len(strategy)):
+            if isinstance(s, int):
+                int_s = strategy[s]
+                strategy[s] = empty_s[:]
+                strategy[s][int_s] = 1
+            elif isinstance(s, list):
+                if not len(s) == len(strategy[s]):
+                    raise Exception("")
+                if not self.has_mixed_strategies:
+                    self.has_mixed_strategies = True
 
     def dominated_strategy(self):
         pass
@@ -14,12 +40,29 @@ class game:
     def game_type(self):
         pass
 
-    #saddle points, Nash equilibrium, or optimal strategies
+    # saddle points, Nash equilibrium, or optimal strategies
     def optimal_strategies(self):
         pass
 
-    def value(self):
-        pass
+    def values(self):
+        vu = min([max(row) for row in self.game_matrix])
+        vl = max([min(column) for column in np.transpose(self.game_matrix)])
+        return {"vl":vl, "vu":vu}
+
+    def mixed_values(self):
+        A = sym.Matrix(self.game_matrix)
+        set = sym.symbols('x1:%d v' % (A.rows + 1))
+        e = sym.Matrix([([1] * A.cols ) + [0]])
+        b = sym.Matrix(([0] * A.rows ) + [1])
+
+        M = (A.row_join(sym.Matrix([-1]*A.cols))).col_join(e)
+        res = sym.linsolve((M,b),set)
+        res = res.args[0]
+        print(res, "  ", type(res))
+        return {str(set[i]):res[i] for i in range(len(set))}
+
+    def detail(self):
+        return str(self.values())+"\n"+str(self.mixed_values())
 
     def pure_strategies(self):
         pass
@@ -27,10 +70,10 @@ class game:
     def best_response(self):
         pass
 
-    def normalize_strategy():
-        pass
 
+# p 27
+M1 = [[1, -1, 1], [-1, 1, -1], [1, -1, 1]]
+M2 = [[3,-1], [-1,9]]
+g = game(M2)
 
-
-
-#p 27
+print(g.detail())
